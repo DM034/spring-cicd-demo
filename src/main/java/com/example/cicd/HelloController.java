1caa257c3d83dc152c78724cbaa32ab806cc2542
maroussiark <soamaroussia@gmail.com>
2026-08-01 13:41:39 +0300

    modfifcation

--- src/main/java/com/example/cicd/HelloController.java
+++ src/main/java/com/example/cicd/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
 
   @GetMapping("/api/hello")
   public Map<String, Object> hello(@RequestParam(name = "name", required = false) String name) {
-    String who = (name == null || name.trim().isEmpty()) ? "Miradooo" : name.trim();
+    String who = (name == null || name.trim().isEmpty()) ? "Maroussia" : name.trim();
 
     Map<String, Object> res = new HashMap<>();
     res.put("message", "Hello " + who + "!");
